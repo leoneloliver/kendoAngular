e52d4de490e7efe82b9c92d24b459de85bc26423
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-04-08 21:20:26 -0400

    bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { products } from './products';
 import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+
+declare var myAct: any;
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,15 @@ import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
   styleUrls: ['./app.component.css']
     
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public data: any[] = products;
   public pageSize = 20;
   public skip = 0;
-  
+
+  ngOnInit() {
+    myAct();
+  }
+
   // public sliderChange(pageIndex: number): void {
   //     this.skip = (pageIndex - 1) * this.pageSize;
   // }
--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@
  function myAct(){
     setTimeout(function(){
      
-     document.getElementById("myGrid").innerHeight =  window.innerHeight;
+     document.getElementsByClassName("k-grid")[0].classList.add("cority");
      //alert(window.innerHeight)
 
     },1000);
